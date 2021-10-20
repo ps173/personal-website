c9710d11b0df32da2591ecd819ce21bf45add11d
ps173 <prathamsharma173@gmail.com>
2021-10-20 11:29:16 +0000

    Deploying to gh-pages from @ ps173/personal-website@2f6a4caf0db8727d77dcd8171da16c79805a5999 🚀

---
+++ _next/static/BchvaQ9T_DUD_NRebzivV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/927-2770edb2eafd742f12cd.js","static/chunks/pages/index-7f6263ec2777a499a77e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
