8d0cd7fc70186009c7353790ab02cf015739125e
ps173 <prathamsharma173@gmail.com>
2021-10-20 16:53:12 +0530

    Adding a title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Posts from '../components/Posts';
@@ -6,6 +7,9 @@ import Projects from '../components/Projects';
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Pratham's Website</title>
+      </Head>
       <Projects />
       <Posts />
       <About />
